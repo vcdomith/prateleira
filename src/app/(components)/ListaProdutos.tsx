a1dfe72b1fbf84949f674eb466097f377de9821a
Vitor Domith <vcdomith@hotmail.com>
2025-03-19 17:42:14 -0300

    initial commit

---
+++ src/app/(components)/ListaProdutos.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { useEffect, useMemo, useState } from "react"
+
+export interface Produto {
+    id: number
+    created_at: Date
+    codigo: string
+    local: string
+    ativo: boolean
+}
+
+export default function ListaProdutos({produtos}: {produtos: Produto[]}) {
+
+    const [search, setSearch] = useState('')
+    const [debouncedSearch, setDebouncedSearch] = useState(search);
+
+    // Debounce logic
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedSearch(search);
+        }, 100); // Adjust the delay as needed
+
+        return () => clearTimeout(timer);
+    }, [search]);
+
+    const produtosDisplay = useMemo(() => produtos.filter( produto => produto.codigo.includes(debouncedSearch.toUpperCase())), [debouncedSearch, produtos])
+
+    console.log(produtosDisplay);
+
+    return (
+        <>
+        <input 
+            type="text" 
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="border border-2px border-solid border-white"
+        />
+        <ul>
+          {produtosDisplay?.map( ({codigo, local}, index) => 
+            <li 
+                key={codigo+local+index}
+                className="flex gap-4"
+            >
+              <h2>{codigo}</h2>
+              <h5>{local}</h5>
+            </li>
+          )}
+        </ul>
+        </>
+    )
+
+}
